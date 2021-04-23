bfc4c2d6f8b39977604261858472cfbc0600593e
alekseik1 <1alekseik1@gmail.com>
2021-04-24 00:29:58 +0300

    feat: init

---
+++ longpoll.py
@@ -0,0 +1,47 @@
+import os
+from time import sleep
+
+import vk_api
+from dotenv import load_dotenv
+from loguru import logger
+from requests.exceptions import ReadTimeout
+from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
+from vk_api.utils import get_random_id
+
+
+load_dotenv()
+
+vk_session = vk_api.VkApi(token=os.environ.get("ACCESS_TOKEN"))
+longpoll = VkBotLongPoll(vk_session, os.environ.get("GROUP_ID"))
+vk = vk_session.get_api()
+
+ADMIN_ID = "92540660"
+
+
+def send_message(to_id, message: str) -> None:
+    vk.messages.send(peer_id=to_id, random_id=get_random_id(), message=message)
+
+
+def handle_message(event):
+    logger.info("new message")
+    message_text, message_id = event.obj.text, event.obj.id
+    user_id = int(event.obj.from_id)
+    send_message(user_id, "hello")
+
+
+def main():
+    for event in longpoll.listen():
+        # Пока только проверяем входящие сообщения
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            handle_message(event)
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            main()
+        except ReadTimeout as e:
+            logger.error(
+                f"lost connection with vk. Retrying in 10 seconds... Error: {e}"
+            )
+            sleep(10)
