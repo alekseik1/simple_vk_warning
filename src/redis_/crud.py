ebd0cd21b4f0e5bd8fd24c3f360d90250a29cddd
alekseik1 <1alekseik1@gmail.com>
2021-05-08 20:06:24 +0300

    fix(redis): release connections back to pool
    Redis can run out of connections if many requests are made. The fix should help to prevent it

--- src/redis_/crud.py
+++ src/redis_/crud.py
@@ -8,8 +8,11 @@ async def get_redis() -> aioredis.Redis:
 
 async def get_password_from_redis(redis: aioredis.Redis, user_id: int) -> str:
     rv = await redis.get(user_id)
+    await redis.quit()
     return (rv or b'').decode('utf-8')
 
 
 async def set_password_for_user(redis: aioredis.Redis, user_id: int, password: str) -> bool:
-    return await redis.set(user_id, password)
+    rv = await redis.set(user_id, password)
+    await redis.quit()
+    return rv
