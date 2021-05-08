d69d2053cb4ccae84946a46eed9c03a6b0dd7746
alekseik1 <1alekseik1@gmail.com>
2021-05-08 17:54:05 +0300

    ref: restrict Redis usage

--- src/bot.py
+++ src/bot.py
@@ -5,7 +5,6 @@ from loguru import logger
 from vkbottle import load_blueprints_from_package
 from vkbottle.bot import Bot
 from dotenv import load_dotenv
-from src.middlewares import RedisMiddleware
 
 load_dotenv('../.env')
 TOKEN = os.environ.get('ACCESS_TOKEN')
@@ -19,5 +18,4 @@ logger.add(sys.stderr, level="DEBUG")
 for bp in load_blueprints_from_package('blueprints'):
     bp.load(bot)
 
-bot.labeler.message_view.register_middleware(RedisMiddleware)
 bot.run_forever()
