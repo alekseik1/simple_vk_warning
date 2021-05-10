b0ddc82f7328491e93059365080056d64459ccd2
alekseik1 <1alekseik1@gmail.com>
2021-05-11 00:23:26 +0300

    refactor: move test preparation to conftest

--- tests/vk_handling/conftest.py
+++ tests/vk_handling/conftest.py
@@ -2,10 +2,21 @@ import datetime
 
 import pytest
 import json
+from pathlib import Path
 from vkbottle import API
 from vkbottle.tools.test_utils import MockedClient
 
+basedir = Path(__file__).parent
 
+
+@pytest.fixture(autouse=True)
+def prepared_blueprints_paths(monkeypatch):
+    monkeypatch.chdir(basedir.parent.parent / 'src')
+    monkeypatch.syspath_prepend(basedir.parent.parent / 'src')
+    yield
+
+
+# Взял это из реального сообщения ВК
 EXAMPLE_MESSAGE_EVENT = {
     "ts": 1,
     "updates": [
@@ -51,16 +62,25 @@ EXAMPLE_MESSAGE_EVENT = {
 
 @pytest.fixture()
 def raw_message_builder():
+    """
+    Собирает сообщение по шаблону при заданных параметрах.
+    При изменениях API вк надо будет изменить шаблон :(
+
+    :return:
+    """
     def build_message(
             text: str,
             from_id: int = 92540660,
             on_datetime: datetime.datetime = datetime.datetime.fromtimestamp(1620678005),
+            n_messages: int = 1
     ):
         rv = EXAMPLE_MESSAGE_EVENT
         rv['updates'][0]['object']['message']['date'] = on_datetime.timestamp()
         rv['updates'][0]['object']['message']['from_id'] = from_id
         rv['updates'][0]['object']['message']['peer_id'] = from_id
         rv['updates'][0]['object']['message']['text'] = text
+        for i in range(n_messages - 1):
+            rv['updates'].append(rv['updates'][-1].copy())
         return rv
     return build_message
 
@@ -73,14 +93,21 @@ def fake_vk_api_message_builder(
         text: str,
         from_id: int = 92540660,
         on_datetime: datetime.datetime = datetime.datetime.fromtimestamp(1620678005),
+        n_messages: int = 1
     ):
+        # Все подобрано как отвечает реальный ВК, поэтому дважды подумайте, прежде чем менять
+        # Забрал с https://github.com/timoniq/vkbottle/blob/c6ebefb/tests/bot_test.py
         def callback(data: dict):
+            # Этот парень нужен
             if "groups.getById" in data["url"]:
                 return {"response": [{"id": 1}]}
+            # И этот тоже
             elif "groups.getLongPollServer" in data["url"]:
                 return {"response": {"ts": 1, "server": "!SERVER!", "key": ""}}
+            # И этот тоже
             elif "!SERVER!" in data["url"]:
-                return raw_message_builder(text=text, from_id=from_id, on_datetime=on_datetime)
+                return raw_message_builder(text=text, from_id=from_id, on_datetime=on_datetime, n_messages=n_messages)
+            # Отправка сообщений со стороны бота.
             elif "messages.send" in data["url"]:
                 target_data = data['data']
                 if 'keyboard' in target_data:
@@ -89,6 +116,8 @@ def fake_vk_api_message_builder(
                     except TypeError:
                         target_data['keyboard'] = target_data['keyboard'].get_json()
                 return json.dumps({"response": {**data['data'], **{"r": 1}}})
+
+        # Создадим API, в котором клиент будет ходить на коллбеки выше ВМЕСТО vk.com
         api = API("token")
         api.http._session = MockedClient(None, callback=callback)
         return api
--- tests/vk_handling/test_unknown_event.py
+++ tests/vk_handling/test_unknown_event.py
@@ -1,9 +1,5 @@
 import pytest
-from pathlib import Path
 from pytest_mock import MockFixture
-
-
-basedir = Path(__file__).parent
 
 
 @pytest.mark.asyncio
@@ -13,11 +9,8 @@ basedir = Path(__file__).parent
 async def test_goes_to_fallback_on_unknown_message(
         text,
         mocker: MockFixture,
-        monkeypatch,
         fake_vk_api_message_builder
 ):
-    monkeypatch.chdir(basedir.parent.parent / 'src')
-    monkeypatch.syspath_prepend(basedir.parent.parent / 'src')
     from src.bot import bot
     bot.api = fake_vk_api_message_builder(text=text)
     mock = mocker.AsyncMock()
